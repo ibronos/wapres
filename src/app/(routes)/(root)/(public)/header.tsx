1315a0e66f090040920c0d41ebed5ec45fe2bf6d
ibronos <8ronosaidi@gmail.com>
2024-04-22 15:05:24 +0700

    upgrade next and prisma

--- src/app/(routes)/(root)/(public)/header.tsx
+++ src/app/(routes)/(root)/(public)/header.tsx
@@ -5,6 +5,8 @@ export default function Header() {
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-light bg-dark px-2">
         <a className="navbar-brand text-white" href="#">Navbar</a>
+        <a className="navbar-brand text-white" href="admin">Dashboard</a>
+        <a className="navbar-brand text-white" href="register-site">Register Site</a>     
       </nav>
     </div>
   )
